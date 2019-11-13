78df95554ce05c518d84264b0752a155bea0539f
assmass13 <33343215+assmass13@users.noreply.github.com>
2019-11-13 15:27:39 +0200

    Update select.sql

--- sql_basics/select.sql
+++ sql_basics/select.sql
@@ -1,20 +1,20 @@
 WITH
   result_table AS (
     select
-		  p.id as id,
-		  p.first_name as first_name, p.last_name as last_name,
-		  count(o.person_id) as total_orders,
-      sum(oi.quantity) as total_items_bought,
-      sum(oi.quantity * (i.price - oi.discount)) as total_money_spent
-	  from `person` as p
-	  left outer join `order` as o
-		  on p.id = o.person_id
-	  left outer join `order_item` as oi
-		  on o.id = oi.order_id
-	  left outer join `item` as i
-		  on oi.item_id = i.id
-	  group by id
-	  order by id
+  	p.id as id,
+  	p.first_name as first_name, p.last_name as last_name,
+  	count(o.person_id) as total_orders,
+	sum(oi.quantity) as total_items_bought,
+	sum(oi.quantity * (i.price - oi.discount)) as total_money_spent
+    from `person` as p
+    left outer join `order` as o
+    	on p.id = o.person_id
+    left outer join `order_item` as oi
+    	on o.id = oi.order_id
+    left outer join `item` as i
+    	on oi.item_id = i.id
+    group by id
+    order by id
   )
 
 select 
